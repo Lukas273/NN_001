bf67d31efcee8d16faebfbf0b236aff211c55dab
Lukas273 <37696792+Lukas273@users.noreply.github.com>
2018-03-23 12:46:35 +0100

    Create deep_versions.py

---
+++ deep_versions.py
@@ -0,0 +1,15 @@
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+
+# graphviz
+import graphviz
+print('Graphviz: %s' % graphviz.__version__)
+
+# pydot
+import pydot
+print('Pydot: %s' % pydot.__version__)
